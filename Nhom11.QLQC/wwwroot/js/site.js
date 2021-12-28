02f892b0a7ff16e18ecb9e3e0f4ac68a49baa95c
vantran1317 <1954052121van@ou.edu.vn>
2021-12-28 22:49:40 +0700

    Them KhachHang

--- Nhom11.QLQC/wwwroot/js/site.js
+++ Nhom11.QLQC/wwwroot/js/site.js
@@ -334,3 +334,120 @@ function addQuangCao_LoaiQuangCao() {
         }
     });
 }
+
+//Khách hàng
+function openKH(id) {
+    if (id != null) {
+        $('#divModal').modal('show');
+        $("#spanMaKH").text(id);
+        var item = null;
+        for (var i = 0; i < dataKH.length; i++) {
+            item = dataKH[i];
+            if (item.MaKH == id) {
+                console.log(item);
+                break;
+            }
+        }
+        $("#txtMaKH").val(item.MaKH);
+        $("#txtTenKH").val(item.TenKH);
+        $("#txtGT").val(item.GT);
+        $("#txtEmailKH").val(item.Email);
+    }
+    else {
+        $('#divModal').modal('show');
+        $("#spanMaKH").text("[Thêm mới]");
+        $("#txtmaKH").val("");
+        $("#txtTenKH").val("");
+        $("#txtGT").val("");
+        $("#txtEmailKH").val("");
+    }
+
+}
+
+function saveKH() {
+    if (($("#spanMaKH").text()) == "[Thêm mới]") {
+        var item = {};
+        item.MaKH = ($("#txtMaKH").val());
+        item.TenKH = $("#txtTenKH").val();
+        item.GT = $("#txtGT").val();
+        item.Email = $("#txtEmailKH").val();
+        var str = JSON.stringify(item);
+        console.log(str);
+        $.ajax({
+            type: "POST",
+            url: "/DoiTac?handler=Add",
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader("XSRF-TOKEN",
+                    $('input:hidden[name="__RequestVerificationToken"]').val());
+            },
+            data: { KH: str },
+            dataType: "json",
+            success: function (res) {
+                console.log(res);
+                if (res.success === true || res.success == true) {
+                    console.log("Them moi thanh cong");
+                    alert("Thêm mới thành công!");
+                    location.reload();
+                }
+            },
+            error: function (jqXHR, textStatus, errorThrow) {
+                alert("Thêm mới thất bại!");
+            }
+        });
+    }
+    else {
+        //sửa
+        var item = {};
+        item.MaKH = ($("#spanMaKH").text());
+        item.TenKH = $("#txtTenKH").val();
+        item.GT = $("#txtGT").val();
+        item.Email = $("#txtEmailKH").val();
+
+        var str = JSON.stringify(item);
+        console.log(str);
+        $.ajax({
+            type: "POST",
+            url: "/DoiTac?handler=Update",
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader("XSRF-TOKEN",
+                    $('input:hidden[name="__RequestVerificationToken"]').val());
+            },
+            data: { KH: str },
+            dataType: "json",
+            success: function (res) {
+                console.log(res);
+                if (res.success === true || res.success == true) {
+                    console.log("Cap nhat thanh cong");
+                    alert("Cập nhật thành công!");
+                    location.reload();
+                }
+            },
+            error: function (jqXHR, textStatus, errorThrow) {
+                alert("Cập nhật thất bại!");
+            }
+        });
+    }
+
+}
+function deleteKH(id) {
+    $.ajax({
+        type: "POST",
+        url: "/DoiTac?handler=Delete",
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader("XSRF-TOKEN",
+                $('input:hidden[name="__RequestVerificationToken"]').val());
+        },
+        data: { KHid: id },
+        dataType: "json",
+        success: function (res) {
+            console.log(res);
+            if (res.success === true || res.success == true) {
+                alert("Xóa thành công!");
+                location.reload();
+            }
+        },
+        error: function (jqXHR, textStatus, errorThrow) {
+            alert("Xóa thất bại!");
+        }
+    });
+}
---
+++ QLQC.DAL/KhachHangDAL.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QLQC.DTO;
+using QLQC.DAL.Models;
+
+namespace QLQC.DAL
+{
+    public class KhachHangDAL
+    {
+        private qlQcaoContext db;
+
+        public KhachHangDAL()
+        {
+            db = new qlQcaoContext();
+        }
+
+        public IList<KhachHangDTO> GetAll()
+        {
+            List<KhachHangDTO> res = new List<KhachHangDTO>();
+            try
+            {
+                var ls = db.KhachHangs.ToList();
+                foreach (var c in ls)
+                {
+                    KhachHangDTO khDto = new KhachHangDTO();
+                    khDto.MaKH = c.MaKh;
+                    khDto.TenKH = c.TenKh;
+                    khDto.GT = c.Gt;
+                    khDto.Email = c.Email;
+                    res.Add(khDto);
+                }
+            }
+            catch (Exception e)
+            {
+                res = null;
+            }
+            return res;
+        }
+
+        //Sửa
+        public bool Update(KhachHangDTO KH)
+        {
+            bool res = false;
+            var c = db.KhachHangs.FirstOrDefault(x => x.MaKh == KH.MaKH);
+            if (c.TenKh != KH.TenKH)
+                c.TenKh = KH.TenKH;
+
+            if (c.Email != KH.Email)
+                c.Email = KH.Email;
+
+            if (c.Gt != KH.GT)
+                c.Gt = KH.GT;
+            try
+            {
+                db.KhachHangs.Update(c);
+                db.SaveChanges();
+                res = true;
+            }
+            catch (Exception)
+            {
+
+                res = false;
+            }
+
+            return res;
+        }
+
+        //Xóa
+        public bool Delete(string KHid)
+        {
+            bool res = false;
+            var c = db.KhachHangs.FirstOrDefault(x => x.MaKh.Trim() == KHid.Trim());
+
+            try
+            {
+                db.KhachHangs.Remove(c);
+                db.SaveChanges();
+                res = true;
+            }
+            catch (Exception ex)
+            {
+
+                res = false;
+            }
+
+            return res;
+        }
+
+        //Thêm
+        public KhachHangDTO Add(KhachHangDTO KH)
+        {
+            KhachHangDTO res = new KhachHangDTO();
+            var c = new KhachHang();
+            c.MaKh = KH.MaKH;
+            c.TenKh = KH.TenKH;
+            c.Email = KH.Email;
+            c.Gt = KH.GT;
+            try
+            {
+                db.KhachHangs.Add(c);
+                db.SaveChanges();
+                res.MaKH = c.MaKh;
+                res.TenKH = c.TenKh;
+                res.GT = c.Gt;
+                res.Email = c.Email;
+            }
+            catch (Exception)
+            {
+
+                res = null;
+            }
+
+            return res;
+        }
+    }
+}
