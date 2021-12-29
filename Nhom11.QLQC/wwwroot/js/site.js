b63c0c58c233da8a62e275562ec00345ff8bf5a1
thai tuong <thaituong001@gmail.com>
2021-12-29 21:22:02 +0700

    Nhan Vien

--- Nhom11.QLQC/wwwroot/js/site.js
+++ Nhom11.QLQC/wwwroot/js/site.js
@@ -451,3 +451,158 @@ function deleteKH(id) {
         }
     });
 }
+
+// Bảng nhân viên 
+function openModalNV(id) {
+    $('#divModalNV').modal('show');
+    if (id != null) {
+        $('#spanIdNv').text(id);
+        var item = null;
+        for (var i = 0; i < dataNv.length; i++) {
+            item = dataNv[i];
+            if (item.MaNv == id) {
+                console.log(item);
+                break;
+            }
+        }
+        $("#txtMaNv").val(item.MaNv);
+        $("#txtTenNv").val(item.TenNv);
+        $("#txtMaNhom").val(item.MaNhom);
+        $("#txtEmail").val(item.Email);
+        $("#txtNgSinh").val(item.NgSinh);
+        $("#txtNgVaoLam").val(item.NgVaoLam);
+        $("#txtGt").val(item.Gt);
+    }
+    else {
+        $('#spanIdNv').text("Thêm mới");
+        $("#txtMaNv").val("");
+        $("#txtTenNv").val("");
+        $("#txtMaNhom").val("");
+        $("#txtEmail").val("");
+        $("#txtNgSinh").val("");
+        $("#txtNgVaoLam").val("");
+        $("#txtGt").val("");
+    }
+}
+function saveNhanVien() {
+    if ($('#spanIdNv').text() == "Thêm mới") {
+
+        var item = {};
+        item.MaNv = $("#txtMaNv").val();
+        item.TenNv = $("#txtTenNv").val();
+        item.MaNhom = $("#txtMaNhom").val();
+        item.Email = $("#txtEmail").val();
+        item.NgSinh = $("#txtNgSinh").val();
+        item.Gt = $("#txtGt").val();
+        item.NgVaoLam = $("#txtNgVaoLam").val();
+        var str = JSON.stringify(item);
+        $.ajax({
+            type: "POST",
+            url: "/NhanVien?handler=Add",
+            //data: item
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader("XSRF-TOKEN",
+                    $('input:hidden[name="__RequestVerificationToken"]').val());
+            },
+            data: { nv: str },
+            dataType: "json",
+            success: function (res) {
+                console.log(res);
+                if (res.success === true || res.success == true) {
+                    console.log(res.nv);
+                    var obj = res.nv;
+                    item.MaNv = obj.maNv;
+                    alert("Thêm mới thành công!");
+                    dataNv.push(item);
+                    $('#spanIdNv').text(obj.maNv);
+                    var htmlStr = '<tr style="text-align:center" id="trNv_' + obj.maNv + '">';
+                    htmlStr = htmlStr + '<td>' + obj.maNv + '</td>';
+                    htmlStr = htmlStr + '<td>' + obj.tenNv + '</td>';
+                    htmlStr = htmlStr + '<td>' + obj.maNhom + '</td>';
+                    htmlStr = htmlStr + '<td>' + obj.ngSinh + '</td>';
+                    htmlStr = htmlStr + '<td>' + obj.ngVaoLam + '</td>';
+                    htmlStr = htmlStr + '<td>' + obj.email + '</td>';
+                    htmlStr = htmlStr + '<td>' + obj.gt + '</td>';
+                    htmlStr = htmlStr + '<td> <button style="margin-bottom:5px" type="button" onclick="openModalNV(' + "'" + obj.maNv + "'" + ');" class="btn btn-outline-primary">Sửa</button>';
+                    htmlStr = htmlStr + '<button type="button"  onclick="deleteNhanVien(' + "'" + obj.maNv + "'" + ');" class="btn btn-outline-danger">Xoá</button> </td>';
+                    htmlStr = htmlStr + '</tr>';
+                    $("#tbNV").append(htmlStr);
+                }
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log(textStatus, errorThrown);
+            }
+        });
+    }
+    else {
+        var item = {};
+        item.MaNv = ($("#spanIdNv").text());
+        item.MaNv = $("#txtMaNv").val();
+        item.TenNv = $("#txtTenNv").val();
+        item.MaNhom = $("#txtMaNhom").val();
+        item.Email = $("#txtEmail").val();
+        item.NgSinh = $("#txtNgSinh").val();
+        item.Gt = $("#txtGt").val();
+        item.NgVaoLam = $("#txtNgVaoLam").val();
+        var str = JSON.stringify(item);
+        //console.log(str);
+        $.ajax({
+            type: "POST",
+            url: "/NhanVien?handler=Update",
+            //data: item
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader("XSRF-TOKEN",
+                    $('input:hidden[name="__RequestVerificationToken"]').val());
+            },
+            data: { nv: str },
+            dataType: "json",
+            success: function (res) {
+                console.log(res);
+                if (res.success === true || res.success == true) {
+                    console.log("Cap nhat thanh cong");
+                    alert("Cập nhật thành công!");
+                    location.reload();
+                }
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log(textStatus, errorThrown);
+            }
+        });
+    }
+}
+function deleteNhanVien(id) {
+    //console.log(id);
+    $.ajax({
+        type: "POST",
+        url: "/NhanVien?handler=Delete",
+        //data: item
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader("XSRF-TOKEN",
+                $('input:hidden[name="__RequestVerificationToken"]').val());
+        },
+        data: { idnv: id },
+        dataType: "json",
+        success: function (res) {
+            console.log(res);
+            if (res.success === true || res.success == true) {
+                //console.log("Xóa thành công");
+                location.reload();
+                //$("#trNv_" + id).remove();
+                var i = 0;
+                for (var i = 0; i < dataNv.length; i++) {
+                    var nhanvien = dataNv[i];
+                    if (nhanvien.MaNv == item.MaNv) {
+                        break;
+                    }
+                }
+                dataNv.splice(i, 1);
+                alert("Xóa thành công!");
+            }
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log(textStatus, errorThrown);
+            alert("Nhân viên hợp đồng với khách hàng nên không xóa được");
+        }
+    });
+
+}
