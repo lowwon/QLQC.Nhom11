0ddfd3a9a0260b6a95bf4823bf7dab7655fd9b3e
Your Name <quochungaskc@gmail.com>
2021-12-31 16:23:43 +0700

    push again 6 trang nc

--- Nhom11.QLQC/Pages/QuangCao_NhanVien.cshtml.cs
+++ Nhom11.QLQC/Pages/QuangCao_NhanVien.cshtml.cs
@@ -12,19 +12,28 @@ namespace Nhom11.QLQC.Pages
     public class QuangCao_NhanVienModel : PageModel
     {
         private QuangCaoBLL bus;
-        private NhanVienBLL bus1;
+        private NhomNhanVienBLL bus1;
+        private NhanVienBLL bus2;
+        private QC_LQCBLL bus3;
         public List<QuangCaoDTO> lst;
-        public List<NhanVienDTO> lst1;
+        public List<NhomNhanVienDTO> lst1;
+        public List<NhanVienDTO> lst2;
+        public List<QC_LQCDTO> lst3;
         public string gt { get; private set; }
+        public string value { get; private set; }
         public QuangCao_NhanVienModel()
         {
             bus = new QuangCaoBLL();
-            bus1 = new NhanVienBLL();
+            bus1 = new NhomNhanVienBLL();
+            bus2 = new NhanVienBLL();
+            bus3 = new QC_LQCBLL();
         }
         public void OnGet()
         {
             lst = null;
             lst1 = null;
+            lst2 = null;
+            lst3 = null;
         }
         public void OnPost()
         {
@@ -34,9 +43,37 @@ namespace Nhom11.QLQC.Pages
             {
                 lst = null;
                 lst1 = null;
+                lst2 = null;
+                lst3 = null;
             }
             else
+            {
+                lst2 = bus2.GetAll().ToList();
+                lst1 = bus1.GetAll().ToList();
                 lst = bus.GetAll().ToList();
+                lst3 = bus3.GetAll().ToList();
+                value = Request.Form["ass"];
+                var temp1 = new List<QuangCaoDTO>();
+                if (gt == "tnv")
+                {
+                    temp1 = (from qc in lst
+                             join nnv in lst1 on qc.MaNhom equals nnv.MaNhom into t
+                             from x in t
+                             join nv in lst2 on x.MaNhom equals nv.MaNhom
+                             where nv.TenNv.Trim() == value.Trim()
+                             select qc).ToList();
+                }
+                else
+                {
+                    temp1 = (from qc in lst
+                             join nnv in lst1 on qc.MaNhom equals nnv.MaNhom into t
+                             from x in t
+                             join nv in lst2 on x.MaNhom equals nv.MaNhom
+                             where nv.MaNv.Trim() == value.Trim()
+                             select qc).ToList();
+                }
+                lst = temp1;
+            }
         }
     }
 }
